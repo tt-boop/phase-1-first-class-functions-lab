dc5ebda6bc005bb2767e66be5378198b122d1d8f
Ayomide Taiwo <tosin0330@gmail.com>
2023-04-06 01:12:30 -0500

    myfiles.md

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  console.log(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']));
+  
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
+  
+  console.log(returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']));
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+  function createFareMultiplier(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  }
+  
+  const fareDoubler = createFareMultiplier(2);
+  
+  console.log(fareDoubler(10)); 
+  
+  const firstTwoDrivers = selectingDrivers[0](['Antonia', 'Nuru', 'Amari', 'Mo']);
+  
+  console.log(firstTwoDrivers); 
+
+  function fareTripler(fare) {
+    return fare * 3;
+  }
+  
+  function selectDifferentDrivers(arrayOfDrivers, fn) {
+    return fn(arrayOfDrivers);
+  }
+  
+  
+  
